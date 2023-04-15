a941cb06cf040b943d0141a3b0fa9c6906085d20
Ahmed A. Atia <113174416+Fla4sh@users.noreply.github.com>
2023-04-15 17:08:26 +0200

    Update 301.py

--- 301.py
+++ 301.py
@@ -1,4 +1,5 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 print('''\
                                                                                     
@@ -13,28 +14,38 @@ print('''\
                      @twitter : fla4sh403\
 ''')
 
+
+
 file_path = input("Please enter your file: ")
 
 valid_urls = []
 
-with open(file_path, "r", encoding="utf-8") as file, open("valid_urls.txt", "w") as valid_file:
+def check_url(url):
+    if not url:
+        return
+
+    with requests.Session() as session:
+        response = session.get(url, allow_redirects=True, timeout=5)
+        redirects = response.history[:5]
+        num_redirects = len(redirects)
+
+    if redirects:
+        final_url = response.url
+        valid_urls.append(url)
+        print(f"The URL {url} redirected {num_redirects} times to {final_url}")
+    else:
+        print(f"The URL {url} did not redirect")
+
+with open(file_path, "r", encoding="utf-8") as file, open("valid_urls.txt", "w") as valid_file, ThreadPoolExecutor(max_workers=10) as executor:
+    futures = []
     for line in file:
         url = line.strip()
+        futures.append(executor.submit(check_url, url))
 
-        if not url:  
-            continue
+    for future in futures:
+        future.result()
 
-        with requests.Session() as session:
-            response = session.get(url, allow_redirects=True, timeout=5)
-            redirects = response.history[:3]  
-            num_redirects = len(redirects)
-
-        if redirects:
-            final_url = response.url
-            valid_urls.append(url)
-            print(f"The URL {url} redirected {num_redirects} times to {final_url}")
-            valid_file.write(f"{url}\n")
-        else:
-            print(f"The URL {url} did not redirect")
+valid_file.write("\n".join(valid_urls))
 
 print(f"Valid URLs saved to valid_urls.txt")
+
