6533bdb84980abe7a972f093e443a8c89de420dd
Ahmed A. Atia <113174416+Fla4sh@users.noreply.github.com>
2023-04-16 05:51:46 +0200

    Update 301.py

--- 301.py
+++ 301.py
@@ -1,6 +1,6 @@
 import requests
 from urllib.parse import urlparse
-from concurrent.futures import ThreadPoolExecutor
+import threading
 
 print('''\
                                                                                     
@@ -23,31 +23,37 @@ def check_url(url):
     if not url:
         return
 
-    with requests.Session() as session:
-        response = session.get(url, allow_redirects=True, timeout=5)
-        redirects = response.history[:5]
-        num_redirects = len(redirects)
+    try:
+        with requests.Session() as session:
+            response = session.get(url, allow_redirects=True, timeout=5)
+            redirects = response.history[:5]
+            num_redirects = len(redirects)
 
-    if redirects:
-        final_url = response.url
-        parsed_url = urlparse(url)
-        parsed_final_url = urlparse(final_url)
-        if parsed_url.netloc != parsed_final_url.netloc:  # check if domains are different
-            valid_urls.append(url)
-            print(f"The URL {url} redirected {num_redirects} times to {final_url}")
+        if redirects:
+            final_url = response.url
+            parsed_url = urlparse(url)
+            parsed_final_url = urlparse(final_url)
+            if parsed_url.netloc != parsed_final_url.netloc:  # check if domains are different
+                valid_urls.append(url)
+                print(f"The URL {url} redirected {num_redirects} times to {final_url}")
+            else:
+                print(f"The URL {url} redirected {num_redirects} times to the same domain ({parsed_url.netloc})")
         else:
-            print(f"The URL {url} redirected {num_redirects} times to the same domain ({parsed_url.netloc})")
-    else:
-        print(f"The URL {url} did not redirect")
+            print(f"The URL {url} did not redirect")
+    except Exception as e:
+        print(f"Error occurred while checking URL: {url}")
+        print(e)
 
-with open(file_path, "r", encoding="utf-8") as file, open("valid_urls.txt", "w") as valid_file, ThreadPoolExecutor(max_workers=10) as executor:
-    futures = []
+with open(file_path, "r", encoding="utf-8") as file, open("valid_urls.txt", "w") as valid_file:
+    threads = []
     for line in file:
         url = line.strip()
-        futures.append(executor.submit(check_url, url))
+        t = threading.Thread(target=check_url, args=(url,))
+        t.start()
+        threads.append(t)
 
-    for future in futures:
-        future.result()
+    for t in threads:
+        t.join()
 
     valid_urls_to_write = [url for url in valid_urls if urlparse(url).netloc != urlparse(file_path).netloc and urlparse(url).netloc.endswith("." + urlparse(file_path).netloc)] # filter valid URLs that redirect to another domain or subdomain
     valid_file.write("\n".join(valid_urls_to_write))
